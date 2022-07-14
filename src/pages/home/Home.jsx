f11a49140cc18028d86eae559701c10577e5739e
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-07-14 09:52:46 +0530

    fix: change font-size

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import "./home.css";
 function Home() {
 	return (
-		<div>
+		<div className="home-page-img">
 			<div className="flex items-center justify-center flex-col">
 				<img className="w-80p" src={Dogo} alt="dogo-bg" />
 			</div>
-			<div className="flex items-center justify-center">
+			<div className="btn-next flex items-center justify-center">
 				<Link to="/videos">
 					<button className="explore-btn text-md mt-4 cursor border-5 p-2 w-40">
 						Explore
